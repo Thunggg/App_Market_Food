c645963775612de38512ed057fade60f01ef8d6d
Thunggg <thuannguyen20041028@gmail.com>
2025-08-10 03:26:19 +0700

    design bottom tabs

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,10 +1,107 @@
+import { APP_COLORS } from "@/utils/constant";
+import AntDesign from "@expo/vector-icons/AntDesign";
+import MaterialCommunityIcons from "@expo/vector-icons/MaterialCommunityIcons";
+import MaterialIcons from "@expo/vector-icons/MaterialIcons";
+import Octicons from "@expo/vector-icons/Octicons";
 import { Tabs } from "expo-router";
 
 const TabLayout = () => {
+  const getIcons = (routeName: string, focused: boolean, size: number) => {
+    if (routeName === "index") {
+      return (
+        <MaterialCommunityIcons
+          name="food-fork-drink"
+          size={size}
+          color={focused ? APP_COLORS.ORANGE : APP_COLORS.GREY}
+        />
+      );
+    }
+    if (routeName === "order") {
+      return (
+        <MaterialIcons
+          name="list-alt"
+          size={size}
+          color={focused ? APP_COLORS.ORANGE : APP_COLORS.GREY}
+        />
+      );
+    }
+
+    if (routeName === "favorite") {
+      return focused ? (
+        <AntDesign name="heart" size={size} color={APP_COLORS.ORANGE} />
+      ) : (
+        <AntDesign name="hearto" size={size} color={APP_COLORS.GREY} />
+      );
+    }
+
+    if (routeName === "notification") {
+      return focused ? (
+        <Octicons name="bell-fill" size={size} color={APP_COLORS.ORANGE} />
+      ) : (
+        <Octicons name="bell" size={size} color={APP_COLORS.GREY} />
+      );
+    }
+
+    if (routeName === "account") {
+      return focused ? (
+        <MaterialCommunityIcons
+          name="account"
+          size={size}
+          color={APP_COLORS.ORANGE}
+        />
+      ) : (
+        <MaterialCommunityIcons
+          name="account-outline"
+          size={size}
+          color={APP_COLORS.GREY}
+        />
+      );
+    }
+
+    return <></>;
+  };
   return (
-    <Tabs screenOptions={{ headerShown: false }}>
-      <Tabs.Screen name="index" />
-      <Tabs.Screen name="setting" />
+    <Tabs
+      screenOptions={({ route }) => ({
+        tabBarIcon: ({ focused, color, size }) => {
+          return getIcons(route.name, focused, size);
+        },
+        headerShown: false,
+        tabBarLabelStyle: { paddingBottom: 3 },
+        tabBarActiveTintColor: APP_COLORS.ORANGE,
+      })}
+    >
+      <Tabs.Screen
+        name="index"
+        options={{
+          title: "Home",
+        }}
+      />
+      <Tabs.Screen
+        name="order"
+        options={{
+          title: "Đơn hàng",
+        }}
+      />
+      <Tabs.Screen
+        name="favorite"
+        options={{
+          title: "Đã thích",
+        }}
+      />
+      <Tabs.Screen
+        name="notification"
+        options={{
+          title: "Thông báo",
+        }}
+      />
+
+      <Tabs.Screen
+        name="account"
+        options={{
+          title: "Tôi",
+        }}
+      />
     </Tabs>
   );
 };
