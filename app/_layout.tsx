770ae67d7c72b39c0c8248c320a0e784af7f60e7
Thunggg <thuannguyen20041028@gmail.com>
2025-08-18 14:16:33 +0700

    add react context

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -11,6 +11,7 @@ import { StatusBar } from "expo-status-bar";
 import "react-native-reanimated";
 import { SafeAreaProvider, SafeAreaView } from "react-native-safe-area-context";
 
+import AppProvider from "@/context/app.context";
 import { useColorScheme } from "@/hooks/useColorScheme";
 import { Keyboard, TouchableWithoutFeedback } from "react-native";
 import { GestureHandlerRootView } from "react-native-gesture-handler";
@@ -28,47 +29,52 @@ export default function RootLayout() {
   return (
     <GestureHandlerRootView>
       <GluestackUIProvider mode="light">
-        <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
-          <SafeAreaProvider>
-            <SafeAreaView
-              className="flex-1"
-              style={{
-                backgroundColor: colorScheme === "light" ? "#fff" : "#000",
-              }}
-              edges={["left", "right"]}
-            >
-              <ThemeProvider
-                value={colorScheme === "light" ? DefaultTheme : DarkTheme}
+        <AppProvider>
+          <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
+            <SafeAreaProvider>
+              <SafeAreaView
+                className="flex-1"
+                style={{
+                  backgroundColor: colorScheme === "light" ? "#fff" : "#000",
+                }}
+                edges={["left", "right"]}
               >
-                <Stack>
-                  <Stack.Screen name="index" options={{ headerShown: false }} />
-                  <Stack.Screen
-                    name="(auth)/signup"
-                    options={{ headerShown: false }}
+                <ThemeProvider
+                  value={colorScheme === "light" ? DefaultTheme : DarkTheme}
+                >
+                  <Stack>
+                    <Stack.Screen
+                      name="index"
+                      options={{ headerShown: false }}
+                    />
+                    <Stack.Screen
+                      name="(auth)/signup"
+                      options={{ headerShown: false }}
+                    />
+                    <Stack.Screen
+                      name="(auth)/login"
+                      options={{ headerShown: false }}
+                    />
+                    <Stack.Screen
+                      name="(auth)/verify"
+                      options={{ headerShown: false }}
+                    />
+                    <Stack.Screen
+                      name="(tabs)"
+                      options={{ headerShown: false }}
+                    />
+                    <Stack.Screen name="+not-found" />
+                  </Stack>
+                  <StatusBar
+                    style="light"
+                    backgroundColor="transparent"
+                    translucent
                   />
-                  <Stack.Screen
-                    name="(auth)/login"
-                    options={{ headerShown: false }}
-                  />
-                  <Stack.Screen
-                    name="(auth)/verify"
-                    options={{ headerShown: false }}
-                  />
-                  <Stack.Screen
-                    name="(tabs)"
-                    options={{ headerShown: false }}
-                  />
-                  <Stack.Screen name="+not-found" />
-                </Stack>
-                <StatusBar
-                  style="light"
-                  backgroundColor="transparent"
-                  translucent
-                />
-              </ThemeProvider>
-            </SafeAreaView>
-          </SafeAreaProvider>
-        </TouchableWithoutFeedback>
+                </ThemeProvider>
+              </SafeAreaView>
+            </SafeAreaProvider>
+          </TouchableWithoutFeedback>
+        </AppProvider>
       </GluestackUIProvider>
     </GestureHandlerRootView>
   );
